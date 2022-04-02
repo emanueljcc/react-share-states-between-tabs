1ee017c4f720700295a498975ddf9efd45e39d2e
Emanuel Castillo <emanuel@asistensi.com>
2022-04-02 16:15:25 -0400

    Updates

---
+++ precache-manifest.07f275a66532994c4fd2545800fb7108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0362647867a9da6bc366d60bf7ae0635",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1ffa934083aff0bdc34",
+    "url": "./static/css/main.5e593df3.chunk.css"
+  },
+  {
+    "revision": "cad12ffac7e54ec02bbb",
+    "url": "./static/js/2.a70bcc47.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.a70bcc47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ffa934083aff0bdc34",
+    "url": "./static/js/main.0d2b5d11.chunk.js"
+  },
+  {
+    "revision": "ca9d7a2de1a0f8f00268",
+    "url": "./static/js/runtime-main.0d77e485.js"
+  }
+]);
